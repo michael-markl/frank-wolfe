2f35726658382892f4875a5d0c5d41a3dabbecc9
Michael Markl <marklmichael98@gmail.com>
2026-03-30 15:09:12 +0200

    Fix set obj val

--- src/frank_wolfe.rs
+++ src/frank_wolfe.rs
@@ -175,7 +175,7 @@ impl<Solution> FrankWolfeResult<Solution> {
 
     pub fn set_obj_val(&mut self, new_obj_val: Float) {
         self.optimality_gap -= self.objective_value - new_obj_val;
-        self.relative_optimality_gap = Self::relative_gap(self.objective_value, new_obj_val);
+        self.relative_optimality_gap = Self::relative_gap(new_obj_val, self.optimality_gap);
         self.objective_value = new_obj_val;
     }
 }
