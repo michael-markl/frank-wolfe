ace69c8a24755b281c68c8e8beef4783a3068fed
Michael Markl <marklmichael98@gmail.com>
2026-09-20 14:56:25 +0200

    Use more iterations, and add sioux-falls dataset with lengths

---
+++ data/sioux-falls-net-with-lengths/README.md
@@ -0,0 +1,13 @@
+This folder contains the version of the Sioux Falls network with supplied length attributes.
+According to the website https://tzin.bgu.ac.il/~bargera/tntp/, published by Dr. Hillel Bar-Gera, accessed 2026-09-20:
+
+> Gregor Laemmel (laemmel@vsp.tu-berlin.de) reports that the first published version of the Sioux-Falls network appears in "Development and Application of a Highway Network Design Model - Volumes 1 and 2," Morlok, E.K., Schofer, J.L., Pierskalla, Marsten, R.E., W.P., Agarwal, S.K., Stoner, J.W., Edwards, J.L., LeBlanc, L.J., and Spacek, D.T., Final Report to the Federal Highway Administration under contract number DOT-FH-11-7862, Department of Civil Engineering, Northwestern University, Evanston, Illinois, July 1973. Link lengths (in miles) are given the following file: [Sioux-Falls Network](http://www.bgu.ac.il/~bargera/tntp/SiouxFalls/SiouxFalls_net1.zip), which is identical to the first version given here in all other attributes. 
+
+The unzipped version of the linked file is located in this folder as `SiouxFalls_net.tnpt`.
+The following license agreement applies to this file; therin the "site host" and "site manager" are those of the website https://tzin.bgu.ac.il/~bargera/tntp.
+
+## License Agreement
+
+DATA SETS ARE FOR ACADEMIC RESEARCH PURPOSES ONLY. USERS ARE FULLY RESPONSIBLE FOR ANY RESULTS OR CONCLUSIONS OBTAINED BY USING THESE DATA SETS. USERS MUST INDICATE THE SOURCE OF ANY DATASET THEY ARE USING IN ANY PUBLICATION THAT RELIES ON ANY OF THE DATASETS PROVIDED IN THIS WEB SITE.
+
+THE SITE HOST AND THE SITE MANAGER ARE NOT RESPONSIBLE FOR THE CONTENT OF THE DATA SETS. AGENCIES, ORGANIZATIONS, INSTITUTIONS AND INDIVIDUALS ACKNOWLEDGED IN THIS WEB SITE FOR THEIR CONTRIBUTION TO THE DATASETS ARE NOT RESPONSIBLE FOR THE CONTENT OR THE CORRECTNESS OF THE DATASETS.
--- run-experiments.sh
+++ run-experiments.sh
@@ -11,6 +11,7 @@ mkdir -p ./results
     time cargo run --release -- carbon-pricing --graph "$NETWORKS/Berlin-Center/berlin-center_net.tntp" \
         --demand "$NETWORKS/Berlin-Center/berlin-center_trips.tntp" \
         --out_csv results/berlin-center-permit-based-cordon.csv --min_price=0 --max_price=10 --min_per_time_unit=0.01666666667 \
+        --max_iter  50000
         --km_per_distance_unit=0.001 --steps 101 --cordon_edge_map ./berlin-center-cordon-edge-map.csv --permit_based
 ) 2>&1 | tee results/berlin-center-permit-based-cordon.log
 
@@ -18,7 +19,9 @@ mkdir -p ./results
 (
     set -x
     time cargo run --release -- carbon-pricing --graph "$NETWORKS/Berlin-Center/berlin-center_net.tntp" \
-    --demand "$NETWORKS/Berlin-Center/berlin-center_trips.tntp" --out_csv results/berlin-center-edge-based-cordon.csv \
+    --demand "$NETWORKS/Berlin-Center/berlin-center_trips.tntp"
+    --out_csv results/berlin-center-edge-based-cordon.csv \
+    --max_iter  50000
     --min_price 0 --max_price 10 --steps 101 --min_per_time_unit 0.01666666667 --km_per_distance_unit=0.001 \
     --cordon_edge_map ./berlin-center-cordon-edge-map.csv
 ) 2>&1 | tee results/berlin-center-edge-based-cordon.log
@@ -27,7 +30,9 @@ mkdir -p ./results
 (
     set -x
     time cargo run --release -- carbon-pricing --graph "$NETWORKS/Berlin-Center/berlin-center_net.tntp" \
-        --demand "$NETWORKS/Berlin-Center/berlin-center_trips.tntp" --out_csv results/berlin-center-carbon-pricing.csv \
+        --demand "$NETWORKS/Berlin-Center/berlin-center_trips.tntp"
+        --out_csv results/berlin-center-carbon-pricing.csv \
+        --max_iter  50000
         --min_price 0 --max_price 10 --steps 101 --min_per_time_unit 0.01666666667 --km_per_distance_unit=0.001
 ) 2>&1 | tee results/berlin-center-carbon-pricing.log
 
@@ -35,6 +40,7 @@ mkdir -p ./results
     set -x
     time cargo run --release -- carbon-pricing --graph "$NETWORKS/Berlin-Friedrichshain/friedrichshain-center_net.tntp" \
     --demand "$NETWORKS/Berlin-Friedrichshain/friedrichshain-center_trips.tntp" \
+    --max_iter  50000 \
     --out_csv results/berlin-friedrichshain-carbon-pricing.csv --min_price 0 --max_price 10 --steps 101 \
     --min_per_time_unit 0.01666666667 --km_per_distance_unit=0.001
 ) 2>&1 | tee results/berlin-friedrichshain-carbon-pricing.log
@@ -43,6 +49,7 @@ mkdir -p ./results
     set -x
     time cargo run --release -- carbon-pricing --graph "$NETWORKS/Berlin-Tiergarten/berlin-tiergarten_net.tntp" \
     --demand "$NETWORKS/Berlin-Tiergarten/berlin-tiergarten_trips.tntp" \
+    --max_iter  50000 \
     --out_csv results/berlin-tiergarten-carbon-pricing.csv --min_price 0 --max_price 10 --steps 101 \
     --min_per_time_unit 0.01666666667 --km_per_distance_unit=0.001
 ) 2>&1 | tee results/berlin-tiergarten-carbon-pricing.log
@@ -51,6 +58,7 @@ mkdir -p ./results
     set -x
     time cargo run --release -- carbon-pricing --graph "$NETWORKS/Berlin-Prenzlauerberg-Center/berlin-prenzlauerberg-center_net.tntp" \
     --demand "$NETWORKS/Berlin-Prenzlauerberg-Center/berlin-prenzlauerberg-center_trips.tntp" \
+    --max_iter  50000 \
     --out_csv results/berlin-prenzlauerberg-center-carbon-pricing.csv --min_price 0 --max_price 10 --steps 101 \
     --min_per_time_unit 0.01666666667 --km_per_distance_unit=0.001
 ) 2>&1 | tee results/berlin-prenzlauerberg-center-carbon-pricing.log
@@ -59,20 +67,23 @@ mkdir -p ./results
 (
     set -x
     time cargo run --release -- carbon-pricing --graph "$NETWORKS/Anaheim/Anaheim_net.tntp" \
-    --demand "$NETWORKS/Anaheim/Anaheim_trips.tntp" --out_csv results/anaheim-carbon-pricing.csv \
+    --demand "$NETWORKS/Anaheim/Anaheim_trips.tntp" --max_iter  50000
+    --out_csv results/anaheim-carbon-pricing.csv \
     --min_price 0 --max_price 10 --steps 101 --km_per_distance_unit=0.0003048
 ) 2>&1 | tee results/anaheim-carbon-pricing.log
 
 (
     set -x
     time cargo run --release -- carbon-pricing --graph "$NETWORKS/Winnipeg/Winnipeg_net.tntp" \
-    --demand "$NETWORKS/Winnipeg/Winnipeg_trips.tntp" --out_csv results/winnipeg-carbon-pricing.csv \
+    --demand "$NETWORKS/Winnipeg/Winnipeg_trips.tntp" --max_iter 50000 \
+    --out_csv results/winnipeg-carbon-pricing.csv \
     --min_price 0 --max_price 100 --steps 101
 ) 2>&1 | tee results/winnipeg-carbon-pricing.log
 
 (
     set -x
-    time cargo run --release -- carbon-pricing --graph "$NETWORKS/SiouxFalls/SiouxFalls_net.tntp" \
-    --demand "$NETWORKS/SiouxFalls/SiouxFalls_trips.tntp" --out_csv results/sioux-falls-carbon-pricing.csv \
-    --min_price 0 --max_price 100 --steps 101 --km_per_distance_unit=0.001 --min_per_time_unit 0.01666666667
+    time cargo run --release -- carbon-pricing --graph "./data/sioux-falls-net-with-lengths/SiouxFalls_net.tntp" \
+    --demand "$NETWORKS/SiouxFalls/SiouxFalls_trips.tntp" --max_iter  50000 \
+    --out_csv results/sioux-falls-carbon-pricing.csv \
+    --min_price 0 --max_price 100 --steps 101 --km_per_distance_unit 1.60934 --min_per_time_unit 0.6
 ) 2>&1 | tee results/sioux-falls-carbon-pricing.log
